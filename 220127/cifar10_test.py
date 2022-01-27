76f9bb8bb7250251f00d404135ceb1303436e320
hipass7 <64785177+hipass7@users.noreply.github.com>
2022-01-27 17:30:32 +0900

    Add files via upload

---
+++ 220127/cifar10_test.py
@@ -0,0 +1,145 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from vgg19 import *
+import numpy as np
+from smote import *
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+learning_rate = 0.001
+training_epochs = 20
+batch_size = 4
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+model = VGG('VGG19').to(device)
+
+criterion = torch.nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+
+if __name__ == '__main__':
+    set = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+
+    trainloader = torch.utils.data.DataLoader(set, batch_size = 50000, num_workers = 2, shuffle = True)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+    train_images, train_labels = iter(trainloader).next()
+
+    train_images = np.array(train_images)
+    train_labels = np.array(train_labels)
+
+    trainset = []
+    trainlabel = []
+
+    cnt = [0] * 10
+    for i in range(50000):
+        if train_labels[i] > 4:
+            if cnt[train_labels[i]] >= 500:
+                continue
+        trainset.append(train_images[i])
+        trainlabel.append(train_labels[i])
+        cnt[train_labels[i]] += 1
+
+    traindata = np.array(trainset)
+    trainlabel = np.array(trainlabel)
+    train_images_tensor = torch.tensor(traindata)
+    train_labels_tensor = torch.tensor(trainlabel, dtype = torch.long)
+    trainset = torch.utils.data.TensorDataset(train_images_tensor, train_labels_tensor)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size = 27500, num_workers = 2, shuffle = True)
+
+    total_batch = len(trainloader)
+    print(total_batch)
+    
+    train_images, train_labels = iter(trainloader).next()
+
+    smote = SMOTE(dims=3072)
+    x, y = smote.fit_generate(train_images, train_labels)
+    y=torch.tensor(y, dtype=torch.long)
+
+    trainset = torch.utils.data.TensorDataset(x, y)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size = batch_size, num_workers = 2, shuffle = True)
+
+    total_batch = len(trainloader)
+    print(total_batch)
+
+    for epoch in range(training_epochs):
+        avg_cost = 0
+
+        for X, Y in trainloader: # 미니 배치 단위로 꺼내온다. X는 미니 배치, Y느 ㄴ레이블.
+            # image is already size of (28x28), no reshape
+            # label is not one-hot encoded
+            X = X.to(device)
+            Y = Y.to(device)
+
+            optimizer.zero_grad()
+            hypothesis = model(X)
+            
+            cost = criterion(hypothesis, Y)
+            cost.backward()
+            optimizer.step()
+
+            avg_cost += cost / total_batch
+
+        print('[Epoch: {:>4}] cost = {:>.9}'.format(epoch + 1, avg_cost))
+
+    PATH = './test.pth'
+    torch.save(model.state_dict(), PATH)
+
+    correct = 0
+    total = 0
+    # 학습 중이 아니므로, 출력에 대한 변화도를 계산할 필요가 없습니다
+    with torch.no_grad():
+        for images, labels in testloader:
+            # 신경망에 이미지를 통과시켜 출력을 계산합니다
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            # 가장 높은 값(energy)를 갖는 분류(class)를 정답으로 선택하겠습니다
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
+
+    # 각 분류(class)에 대한 예측값 계산을 위해 준비
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+
+    # 변화도는 여전히 필요하지 않습니다
+    with torch.no_grad():
+        for images, labels in testloader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            _, predictions = torch.max(outputs, 1)
+            # 각 분류별로 올바른 예측 수를 모읍니다
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+
+
+    # 각 분류별 정확도(accuracy)를 출력합니다
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print("Accuracy for class {:5s} is: {:.1f} %".format(classname,
+                                                    accuracy))
+
